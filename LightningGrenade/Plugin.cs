ed012fbc855b3f83f9a4ec9aa6f534f355dba1f4
jacobot1 <155650471+jacobot1@users.noreply.github.com>
2025-09-10 01:14:09 -0500

    bump version

--- LightningGrenade/Plugin.cs
+++ LightningGrenade/Plugin.cs
@@ -13,7 +13,7 @@ namespace LightningGrenade
         // Mod metadata
         public const string modGUID = "com.jacobot5.LightningGrenade";
         public const string modName = "LightningGrenade";
-        public const string modVersion = "2.0.0";
+        public const string modVersion = "2.0.1";
 
         // Initalize Harmony
         private readonly Harmony harmony = new Harmony(modGUID);
